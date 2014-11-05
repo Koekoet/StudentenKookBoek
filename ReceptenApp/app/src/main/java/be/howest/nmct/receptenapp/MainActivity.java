48b263d44430677afd446401921214e872622b0c
Tijs De Lameillieure <tijs.de.lameillieure@student.howest.be>
2014-11-05 09:40:11 +0100

    testje

--- ReceptenApp/app/src/main/java/be/howest/nmct/receptenapp/MainActivity.java
+++ ReceptenApp/app/src/main/java/be/howest/nmct/receptenapp/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//commentaar van Tijs
+//commentaar van Tijs blablablablalkjmqsdlmfjklskmqf
+//msdkfjqlmksqdjfmqslkdfjmqlsdkjmlkqjdlkfmqslmkdflmkqsflksqdmlkf
+//psqdfjp
 public class MainActivity extends Activity {
 
     //THIS IS KOEKOOEEEET!
